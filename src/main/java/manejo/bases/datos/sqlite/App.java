ddab89a88079a4176345513a01c6c9c6ac901107
Carlos E Restrepo F <cerf_fx@yahoo.com>
2021-08-09 14:27:45 -0500

    commit inic 1

--- src/main/java/manejo/bases/datos/sqlite/App.java
+++ src/main/java/manejo/bases/datos/sqlite/App.java
@@ -4,8 +4,11 @@ package manejo.bases.datos.sqlite;
  * Hello world!
  *
  */
+
 public class App 
 {
+
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
